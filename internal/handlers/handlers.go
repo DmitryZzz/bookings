cc4bba62ce8497ed02fcfe2eeae339edb60a5cac
DmitryZzz <yes.zimin@gmail.com>
2022-01-16 01:01:32 +0300

    Server side form validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/DmitryZzz/bookings/internal/config"
+	"github.com/DmitryZzz/bookings/internal/forms"
 	"github.com/DmitryZzz/bookings/internal/models"
 	"github.com/DmitryZzz/bookings/internal/render"
 )
@@ -51,7 +52,14 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and display form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page
