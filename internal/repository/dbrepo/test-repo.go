206f1102bf19e0ae0bf626f0b2585a65d67b904b
DmitryZzz <yes.zimin@gmail.com>
2022-02-19 23:46:43 +0300

    added authorisation

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -70,3 +70,22 @@ func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
 	room = models.Room{ID: 1, RoomName: "General`s Quarters"}
 	return room, nil
 }
+
+// GetUserByID returns a user by id
+func (m *testDBRepo) GetUserByID(id int) (models.User, error) {
+	var u models.User
+	return u, nil
+}
+
+// UpdateUser updates a user in the database
+func (m *testDBRepo) UpdateUser(u models.User) error {
+	return nil
+}
+
+// Authenticate authenticates a user
+func (m *testDBRepo) Authenticate(email, testPassword string) (int, string, error) {
+	var id int
+	var hashedPassword string
+	return id, hashedPassword, nil
+}
+
